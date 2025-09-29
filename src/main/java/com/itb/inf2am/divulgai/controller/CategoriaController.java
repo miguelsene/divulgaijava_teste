ab5a138401342caa913af70dcde22589a38473ac
gabrielfpimenta <rm93995@estudante.fieb.edu.br>
2025-09-29 12:30:06 -0300

    $ métodos

--- src/main/java/com/itb/inf2am/divulgai/controller/CategoriaController.java
+++ src/main/java/com/itb/inf2am/divulgai/controller/CategoriaController.java
@@ -3,15 +3,13 @@
     import com.itb.inf2am.divulgai.model.entity.Categoria;
     import com.itb.inf2am.divulgai.model.services.CategoriaService;
     import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.web.bind.annotation.GetMapping;
-    import org.springframework.web.bind.annotation.RequestMapping;
-    import org.springframework.web.bind.annotation.RestController;
-    import org.springframework.web.bind.annotation.PostMapping;
-    import org.springframework.web.bind.annotation.RequestBody;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
 
 
     import java.util.ArrayList;
     import java.util.List;
+    import java.util.Map;
 
     // Getter (get): Apenas lê o valor do atributo.
     // Setter (set): Apenas modifica o valor do atributo.
@@ -32,5 +30,95 @@
         public Categoria create(@RequestBody Categoria categoria) {
             return categoriaService.save(categoria); // chama o service
         }
+
+
+        @GetMapping("/{id}")
+        public ResponseEntity<Object> listarProdutoPorId(@PathVariable String id) {
+            try {
+                return ResponseEntity.ok(categoriaService.findById(Long.parseLong(id)));
+            } catch (NumberFormatException e) {
+                return ResponseEntity.badRequest().body(
+                        Map.of(
+                                "status", 400,
+                                "error", "Bad Request",
+                                "message", "O id informado não é válido: " + id
+                        )
+                );
+
+
+            } catch (RuntimeException e) {
+                return ResponseEntity.status(404).body(
+                        Map.of(
+                                "status", 404,
+                                "error", "Not Found",
+                                "message", "Produto não encontrado com o id " + id
+                        )
+
+                );
+
+            }
+
+
+        }
+
+        @PutMapping("/{id}")
+        public ResponseEntity<Object> atualizarProduto(@PathVariable String id, @RequestBody Categoria categoria) {
+            try {
+                return ResponseEntity.ok(categoriaService.findById(Long.parseLong(id)));
+            } catch (NumberFormatException e) {
+                return ResponseEntity.badRequest().body(
+                        Map.of(
+                                "status", 400,
+                                "error", "Bad Request",
+                                "message", "O id informado não é válido: " + id
+                        )
+                );
+
+
+            } catch (RuntimeException e) {
+                return ResponseEntity.status(404).body(
+                        Map.of(
+                                "status", 404,
+                                "error", "Not Found",
+                                "message", "Produto não encontrado com o id " + id
+                        )
+
+                );
+
+            }
+
+
+        }
+
+
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Object> excluirProduto(@PathVariable String id, @RequestBody Categoria categoria) {
+            try {
+                return ResponseEntity.ok(categoriaService.findById(Long.parseLong(id)));
+            } catch (NumberFormatException e) {
+                return ResponseEntity.badRequest().body(
+                        Map.of(
+                                "status", 400,
+                                "error", "Bad Request",
+                                "message", "O id informado não é válido: " + id
+                        )
+                );
+
+
+            } catch (RuntimeException e) {
+                return ResponseEntity.status(404).body(
+                        Map.of(
+                                "status", 404,
+                                "error", "Not Found",
+                                "message", "Produto não encontrado com o id " + id
+                        )
+
+                );
+
+            }
+
+
+        }
+
     }
 
--- src/main/java/com/itb/inf2am/divulgai/model/services/CategoriaService.java
+++ src/main/java/com/itb/inf2am/divulgai/model/services/CategoriaService.java
@@ -12,11 +12,12 @@ import java.util.List;
 public class CategoriaService {
 
     @Autowired       // Injeção de dependência
-    private CategoriaRepository CategoriaRepository;
+    private CategoriaRepository categoriaRepository;
 
     // Método responsável em listar todos os Categorias cadastrados no banco de dados
     //READ DO CRUD
     public List<Categoria> findAll() {
+
         return CategoriaRepository.findAll();
     }
 
@@ -25,7 +26,28 @@ public class CategoriaService {
     public Categoria save(Categoria Categoria) {
         Categoria.setStatusCategoria(true);
         return CategoriaRepository.save(Categoria);
+    }
+
+        // Método responsável em listar o produto por ID
+        public Categoria findById (Long id) {
+            return categoriaRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Produto não encontrado com o id " + id));
+        }
+
+        // Método responsável em atualizar em atualizar o produto
+        public Categoria update(Long id, Categoria categoria) {
+            Categoria categoriaExistente = findById(id);
+            categoriaExistente.setNome(categoria.getNome());
+            categoriaExistente.setStatusCategoria(categoria.isStatusCategoria());
+
+            return categoriaRepository.save(categoriaExistente);
+        }
+
+        // Método responsável em excluir o produto ( exclusão física )
+        public void delete(Long id) {
 
 
+            Categoria categoriaExistente = findById(id);
+            categoriaRepository.delete(categoriaExistente);
     }
 }
